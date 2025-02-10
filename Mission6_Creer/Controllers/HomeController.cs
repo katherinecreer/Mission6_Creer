960f6bec4986e8c9229f91074241cfa11bf46df7
katherinecreer <kac263@byu.edu>
2025-02-10 10:45:16 -0700

    Class 2/10/25 work

--- Mission6_Creer/Controllers/HomeController.cs
+++ Mission6_Creer/Controllers/HomeController.cs
@@ -18,7 +18,12 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult AboutJoel()
+    {
+        return View();
+    }
+    
+    public IActionResult Movies()
     {
         return View();
     }
