8434c1e1258fefbcce8e3369418bd97e1e2b1586
katherinecreer <kac263@byu.edu>
2025-02-10 10:19:19 -0700

    initial commit

---
+++ Mission6_Creer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission6_Creer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
